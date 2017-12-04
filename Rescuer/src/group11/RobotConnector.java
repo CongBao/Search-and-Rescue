ce7b3d4a5900e38689b6b8915e3247b3e53bcc26
Frank Han <frankhcy@foxmail.com>
2017-12-04 07:50:56 +0000

    connection part
    works on my machine. not sure on ev3, going to test it today

---
+++ Rescuer/src/group11/RobotConnector.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Robot Connector
+ * @author frank
+ */
+public class RobotConnector extends Thread {
+    
+    private PilotRobot robot;
+    
+    /**
+     * Constructor
+     * @param robotInput the robot controller
+     */
+    public RobotConnector(PilotRobot robotInput) {
+        robot = robotInput;
+    }
+    
+    /**
+     * The runnable
+     */
+    @Override
+    public void run() {
+        try {
+            ServerSocket server = new ServerSocket(21900);
+            Socket client;
+            PrintStream sendOut;
+            BufferedReader recieved;
+            while(true) {
+                client = server.accept();
+                sendOut = new PrintStream(client.getOutputStream());
+                recieved =  new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String str = recieved.readLine();
+                String[] parsed = str.split(":");
+                switch(parsed[0]) {
+                    case "GO":
+                        robot.getPilot().travel(Double.parseDouble(parsed[1]));
+                        while(robot.getPilot().isMoving()) {
+                            Thread.yield();
+                        }
+                        sendOut.println("");
+                        break;
+                    case "TURN":
+                        robot.getPilot().rotate(Double.parseDouble(parsed[1]));
+                        while(robot.getPilot().isMoving()) {
+                            Thread.yield();
+                        }
+                        sendOut.println("");
+                        break;
+                    case "COLOUR":
+                        sendOut.println(String.valueOf(robot.getColour()));
+                        break;
+                    case "DISTANCE":
+                        sendOut.println(String.valueOf(robot.getDistance()));
+                        break;
+                }
+                sendOut.close();  
+                client.close();
+            }
+        } catch(IOException e) {
+            
+        }
+    }
+}
